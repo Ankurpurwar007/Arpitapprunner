132d1b5f0b30d24ed9d05fb005ab538f2d1681a8
Arpit Agarwal <arpitagarwal1308@gmail.com>
2022-12-14 12:14:12 +0530

    Response from both AR services

--- server.py
+++ server.py
@@ -10,10 +10,14 @@ def hello_world(request):
     if name == None or len(name) == 0:
         name = "world"
     message = "Hello, " + name + "!\n"
-    response = requests.get("https://noderestapi.agaarpi.awsps.myinstance.com/api/user/users")
-    print(response.text)
+    response1 = requests.get("https://r2xqadrzpk.us-east-2.awsapprunner.com/api/user/users")
+    print(response1.text)
     print(message)
-    return Response(message + response.text)
+    response2 = requests.get("https://unhaw3wrck.us-east-2.awsapprunner.com/api/user/users")
+    print(response2.text)
+    print(message)
+    return Response(message + response1.text + response2.text)
+    
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
