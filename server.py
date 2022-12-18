a387093af3fb203c5bf430e6f98f5d6d59781427
Arpit Agarwal <arpitagarwal1308@gmail.com>
2022-12-18 16:24:00 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
-import requests
-
 import os
 
 def hello_world(request):
@@ -10,14 +8,7 @@ def hello_world(request):
     if name == None or len(name) == 0:
         name = "world"
     message = "Hello, " + name + "!\n"
-    response1 = requests.get("https://r2xqadrzpk.us-east-2.awsapprunner.com/api/user/users")
-    print(response1.text)
-    print(message)
-    response2 = requests.get("https://unhaw3wrck.us-east-2.awsapprunner.com/api/user/users")
-    print(response2.text)
-    print(message)
-    return Response(message + response1.text + response2.text)
-    
+    return Response(message)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
