f1afa7e4b418301245fa6c63d98b9b38282ecccb
Arpit Agarwal <arpitagarwal1308@gmail.com>
2023-01-03 12:10:00 +0530

    Update server.py

--- server.py
+++ server.py
@@ -12,6 +12,7 @@ def hello_world(request):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
+    print("Application Running on port: ", port)
     with Configurator() as config:
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
