0c36894877871655910e19a0a8699e52ea4f97e3
Arpit Agarwal <arpitagarwal1308@gmail.com>
2023-01-03 12:18:06 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,17 +2,15 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 import os
-
+port = int(os.environ.get("PORT"))
 def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
-    message = "Hello, " + name + "!\n"
+    message = "Hello, " + name + str(port) + "!\n"
     return Response(message)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT"))
-    print("Application Running on port: ", port)
     with Configurator() as config:
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
