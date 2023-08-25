b20474e59ec7f41f5876f78660faa121f70e849f
Arpit Agarwal <arpitagarwal1308@gmail.com>
2023-08-25 14:15:36 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,13 +2,17 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 import os
+import socket
+import logging
 port = int(os.environ.get("PORT"))
+data = socket.gethostbyname_ex("www.google.com")
 def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
     message = "Hello, " + name + str(port) + "!\n"
-    return Response(message)
+    logging.debug("\n\nThe IP Address of the Domain Name is: "+repr(data))
+    return Response(message + "\n\nThe IP Address of the Domain Name is: "+repr(data))
 
 if __name__ == '__main__':
     with Configurator() as config:
